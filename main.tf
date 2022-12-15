ad5197b18e0bcc5ec9f648bfc3e4ef5489b1de5f
José Romário Viana Filho <romarioviana@yahoo.com.br>
2022-12-15 20:20:43 +0000

    feat: criando instancia na AWS com Terraform IaC

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "us-west-2"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-0ecc74eca1d66d8a6"
+  instance_type = "t2.micro"
+  key_name = "isc-chave-nuvem"
+  tags = {
+    Name = "PrimeiraInstancia"
+  }
+}
+
